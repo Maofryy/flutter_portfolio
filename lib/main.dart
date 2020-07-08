b0b050951b408aeee9de2f098012f9ab7737f91f
Maori Benhassine <maori.benhassine@gmail.com>
2020-07-08 20:49:15 +0200

    change home page label

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Portfolio Home Test Page'),
+      home: MyHomePage(title: 'Welcome to the Home Test Page'),
     );
   }
 }
